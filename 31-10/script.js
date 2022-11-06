52735eea1f40d3ad3e793449dd5c18229ac72486
Macho <turdumatovmarat04x@gmail.com>
2022-11-06 22:14:24 +0600

    fead:add task

---
+++ 31-10/script.js
@@ -0,0 +1,20 @@
+let userSalaries = {
+    Ella: 1000,
+    Sophia: 2000,
+    Ellie: 3000
+};
+console.log(userSalaries.Ella);
+console.log(userSalaries.Sophia);
+
+
+let count = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+console.log(count.a + count.b + count.c);
+
+let data = 1;
+let number;
+number = data ? 3 : 5;
+console.log(number);
